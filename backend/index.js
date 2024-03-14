853448e598ee60b57f634d6a5af6709f045779ae
chalana kalpitha <chalanakalpitha72@gmail.com>
2024-03-14 20:15:42 +0530

    add defs and resolvers to index file

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import mergeTypeDef from "./typeDefs/index.js";
+import mergeResolver from "./resolvers/index.js";
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  typeDefs: mergeTypeDef,
+  resolvers: mergeResolver,
 });
 
 const { url } = await startStandaloneServer(server);
--- backend/resolvers/index.js
+++ backend/resolvers/index.js
@@ -1,8 +1,8 @@
-import { mergeResolvers, mergeResolvers } from "@graphql-tools/merge";
+import { mergeResolvers } from "@graphql-tools/merge";
 
-import userResolver from "./user.resolver";
-import transactionResolver from "./transaction.resolver";
+import userResolver from "./user.resolver.js";
+import transactionResolver from "./transaction.resolver.js";
 
-const mergeResolvers = mergeResolvers([userResolver,transactionResolver]);
+const mergeResolver = mergeResolvers([userResolver,transactionResolver]);
 
-export default mergeResolvers
+export default mergeResolver
